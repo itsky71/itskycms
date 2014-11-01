79a4fd73a638e437352043e89f8ea0a2f8c29dfa
itsky71 <itsky71@foxmail.com>
2014-11-02 00:37:21 +0800

    .gitignore is now working

--- ITskyCMS/Application/Admin/Lang/zh-cn/menu_common.php
+++
@@ -1,8 +0,0 @@
-<?php
-$menu_common = array(
-    'SET'           =>  '设置',
-    'PROFILE'       =>  '个人资料',
-    'MENU_DASHBOARD'=>  '控制台',
-    'MENU_INDEX'    =>  '首页',
-    'MENU_LOOK'     =>  '查看'
-);
--- ITskyCMS/Application/Admin/Lang/zh-tw.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-    'LOGIN_SUCCESS' =>  '登入成功!',
-    'LOGIN_ERROR'   =>  '用戶名或密碼錯誤!',
-    'VERIFY_ERROR'  =>  '驗證碼錯誤!'
-);
