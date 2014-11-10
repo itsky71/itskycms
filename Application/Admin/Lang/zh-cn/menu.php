9607986b46d69e2cf27443bbebca3ea9900bc6b1
itsky71 <itsk71@foxmail.com>
2014-11-11 00:15:40 +0800

    后台管理菜单

--- Application/Admin/Lang/zh-cn/menu.php
+++ Application/Admin/Lang/zh-cn/menu.php
@@ -5,5 +5,10 @@ return array(
     'MENU_LIST' =>  '后台管理菜单列表',
     'PARENT_ID' =>  '上级',
     'ADD'       =>  '添加菜单',
-    'REAL_NAME' =>  '名称'
+    'REAL_NAME' =>  '名称',
+    'MODEL'     =>  '模块',
+    'ACTION'    =>  '方法',
+    'DATA'      =>  '参数',
+    'REMARK'    =>  '备注',
+    'STATUS'    =>  '状态'
 );
