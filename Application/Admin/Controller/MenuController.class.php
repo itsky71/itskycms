9a4545c505bd2034c4698c3bffe668dce363c4e1
itsky71 <>
2014-11-15 22:40:58 +0800

    后台管理菜单

--- Application/Admin/Controller/MenuController.class.php
+++ Application/Admin/Controller/MenuController.class.php
@@ -6,27 +6,74 @@
  */
 namespace Admin\Controller;
 class MenuController extends AdminController{
-        public function add(){
+    public function add(){
         if(IS_POST){
             $Menu = D('Menu');
             if($Menu->create()){
                 $data = array(
                     'pid' => I('post.pid'),
-                    'name' => strtoupper(I('post.model')).'_'.strtoupper(I('post.action') ? I('post.action') : 'index'),
+                    'name' => 'M_'.strtoupper(I('post.model')).'_'.strtoupper(I('post.action') ? I('post.action') : 'index'),
                     'model' => ucfirst(I('post.model')),
-                    'action' => I('post.action') ? I('post.action') : 'index',
+                    'action' => I('post.action') ? strtolower(I('post.action')) : 'index',
                     'data' => I('post.data'),
                     'remark' => I('post.remark'),
                     'status' => I('post.status') ? 1 : 0,
-                    'listorder'=>0
+                    'listorder' => 0
                 );
-                echo LANG_PATH.'  --  '.LANG_SET;
-                print_r($data);
+                if($Menu->add($data)){
+                    $rname = I('post.realname');
+                    $this->_write_lang(array($data['name']=>$rname[LANG_SET]));
+                    $this->success(L('ADD_SUCCESS'),U('Menu/index'));
+                }else{
+                    $this->error(L('ADD_ERROR'));
+                }
             }else{
                 $this->error($Menu->getError());
             }
         }else{
             $this->display('edit');
         }
+    }
+    /**
+     * 验证菜单
+     */
+    public function checkAction(){
+        if(IS_POST){
+            $Menu = D('Menu');
+            $map = array(
+                'model' => ucfirst(I('post.model')),
+                'action' => strtolower(I('post.action'))
+            );
+            $res = $Menu->where($map)->find();
+            if($res){
+                echo 'error';
+            }
+        }else{
+            $this->error(L('_ERROR_ACTION_'));
+        }
+    }
+
+    /**
+     * 编写语言文件
+     * @param array $lang 语言数组
+     * @param string $langpath 语言文件路径
+     * @return boolean
+     */
+    private function _write_lang($lang,$del = FALSE,$langpath = ''){
+        if(!is_array($lang)){
+            return FALSE;
+        }
+        $path = $langpath == '' ? MODULE_PATH.'Lang/'.LANG_SET.'/menu_commonb.php' : $langpath;
+        $phpstar = '<?php'.PHP_EOL;
+        $langdata = $phpstar;
+        $langdata .= '$menu_common = array('.PHP_EOL;
+        $menu_common = array();
+        eval(str_replace($phpstar,'', read_file($path)));
+        $arrdata = array_merge($menu_common, $lang);
+        foreach ($arrdata as $key => $value){
+            $langdata .= "    '$key' => '$value',".PHP_EOL;
+        }
+        $langdata .= ');';
+        return write_file($path,$langdata);
     }
 }
--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -19,4 +19,44 @@ function sys_md5($str,$key=''){
     $key = $key.C('DATA_AUTH_KEY');
     return md5(sha1($str).$key);
 }
-
+/**
+ * 写入文件
+ * @param string $path 文件路径
+ * @param string $data 文件数据
+ * @param string $mode 模式
+ * @return boolean
+ */
+function write_file($path,$data,$mode = 'wb'){
+    if(!$fp = fopen($path, $mode)){
+        return FALSE;
+    }
+    flock($fp, LOCK_EX);
+    fwrite($fp, $data);
+    flock($fp, LOCK_UN);
+    fclose($fp);
+    return TRUE;
+}
+/**
+ * 读取文件
+ * @param string $file 文件路径
+ * @return boolean
+ */
+function read_file($file){
+    if(!file_exists($file)){
+        return FALSE;
+    }
+    if(function_exists('file_get_contents')){
+        return file_get_contents($file);
+    }
+    if(!$fp = fopen($file,'rb')){
+        return FALSE;
+    }
+    flock($fp, LOCK_SH);
+    $data = '';
+    if(filesize($file)>0){
+        $data = &fread($fp, filesize($file));
+    }
+    flock($fp, LOCK_UN);
+    fclose($fp);
+    return $data;
+}
