4f6ff8fc9609c1ba0cdb69b681a8aca9a5eee352
itsky71 <itsk71@foxmail.com>
2014-12-20 10:25:23 +0800

    ThinkPHP 升级

--- ThinkPHP/Library/Think/Auth.class.php
+++ ThinkPHP/Library/Think/Auth.class.php
@@ -79,15 +79,15 @@ class Auth{
     );
 
     public function __construct() {
+        if (C('AUTH_CONFIG')) {
+            //可设置配置项 AUTH_CONFIG, 此配置项为数组。
+            $this->_config = array_merge($this->_config, C('AUTH_CONFIG'));
+        }
         $prefix = C('DB_PREFIX');
         $this->_config['AUTH_GROUP'] = $prefix.$this->_config['AUTH_GROUP'];
         $this->_config['AUTH_RULE'] = $prefix.$this->_config['AUTH_RULE'];
         $this->_config['AUTH_USER'] = $prefix.$this->_config['AUTH_USER'];
         $this->_config['AUTH_GROUP_ACCESS'] = $prefix.$this->_config['AUTH_GROUP_ACCESS'];
-        if (C('AUTH_CONFIG')) {
-            //可设置配置项 AUTH_CONFIG, 此配置项为数组。
-            $this->_config = array_merge($this->_config, C('AUTH_CONFIG'));
-        }
     }
 
     /**
