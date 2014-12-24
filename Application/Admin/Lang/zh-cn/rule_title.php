0567310c674f2f7ff0697f9c1923931bc68a26b9
itsky71 <>
2014-12-24 17:59:37 +0800

    Revert "ThinkPHP 升级版本为2.2.3"
    This reverts commit f0c1a090596f57e3e259ff799d24cccf3deda325.

--- Application/Admin/Lang/zh-cn/rule_title.php
+++
@@ -1,6 +0,0 @@
-<?php
-$rule_title = array(
-    'R_DFDFDF_DFD' => '电饭',
-    'R_DFDFDF_HEHE' => '呵呵',
-    'R_DFDFDF_GFGF' => '多个',
-);
--- Application/Admin/Model/AuthRuleModel.class.php
+++ Application/Admin/Model/AuthRuleModel.class.php
@@ -18,11 +18,25 @@ use Think\Model;
 class AuthRuleModel extends Model{
     //表单验证
     protected $_validate = array(
-        array('group','require','{%GROUPR}'),
-        array('name','3,20','{%NAMEL}',self::EXISTS_VALIDATE,'length'),
-        array('name','english','{%NAMEC}'),
-        array('name','','{%NAMEU}',self::EXISTS_VALIDATE,'unique'),
-        array('title','2,20','{%TITLEL}',self::EXISTS_VALIDATE,'length'),
-        array('condition','0,100','{%CONDITIONL}',self::EXISTS_VALIDATE,'length')
+        array('name','7,50','{%NAMEL}',self::EXISTS_VALIDATE,'length'),
+        array('name','checkName','{%NAMEC}',self::EXISTS_VALIDATE,'callback')
     );
+    //自动完成
+    protected $_auto = array(
+        array('name','changeName',self::MODEL_BOTH,'callback')
+    );
+    //名称验证
+    protected function checkName(){
+        $name = I('post.name');
+        $maarr = explode('/', $name);
+        if(count($maarr) != 2) return FALSE;
+        if(strlen($maarr[0]) < 3 || !preg_match('/^[A-Za-z]+$/',$maarr[0])) return FALSE;
+        if(strlen($maarr[1]) < 3 || !preg_match('/^[A-Za-z]+$/',$maarr[1])) return FALSE;
+        return TRUE;
+    }
+    //转换名称
+    protected function changeName($str){
+        $maarr = explode('/', $str);
+        return ucwords($maarr[0]).'/'.$maarr[1];
+    }
 }
